83c0a4c4eb40ba12a82ca07a670062f6ba9cb11e
dashfirst <41899619+dashfirst@users.noreply.github.com>
2018-08-03 04:16:53 +0300

    Update dashfirst_install.sh

--- dashfirst_install.sh
+++ dashfirst_install.sh
@@ -201,7 +201,7 @@ echo -e "Installing required packages, it may take some time to finish.${NC}"
 apt-get update >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 libdb4.8-dev libdb4.8++-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils \
-install libboost-all-dev libdb5.3++>/dev/null 2>&1
+libboost-all-dev libdb5.3++>/dev/null 2>&1
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
