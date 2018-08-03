8ac5b1f1bb794f7f8d1b707e571a8a1fce82b889
dashfirst <41899619+dashfirst@users.noreply.github.com>
2018-08-03 04:38:45 +0300

    Update dashfirst_install.sh

--- dashfirst_install.sh
+++ dashfirst_install.sh
@@ -21,14 +21,10 @@ NC='\033[0m'
 
 function download_node() {
   echo -e "Prepare to download $COIN_NAME binaries"
-  cd $TMP_FOLDER
-  wget -q $COIN_TGZ
-  tar xvzf $COIN_ZIP >/dev/null 2>&1
-  compile_error
-  strip $COIN_DAEMON $COIN_CLI
-  cp $COIN_DAEMON $COIN_CLI $COIN_PATH
-  cd - >/dev/null 2>&1
-  rm -rf $TMP_FOLDER >/dev/null 2>&1
+  cd /
+  git clone https://github.com/dashfirst/dashfirst
+  cd /dashfirst
+  sh build_no_qt.sh
 }
 
 
